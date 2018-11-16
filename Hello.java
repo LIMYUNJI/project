ec442bbab0d11c81c8500ce1245a3ef3c82e5750
LIMYUNJI <yeinlim12@naver.com>
2018-11-16 16:21:17 +0900

    Add_pj_Hello.java

---
+++ Hello.java
@@ -0,0 +1,24 @@
+
+/**
+ * Hello 프로그래밍
+ * @author (2017315024 임윤지) 
+ * @version (2018.03.12)
+ */
+public class Hello
+{
+    public static int sum(int n, int m)
+    {
+        return n + m;
+    }
+    public static void main(String[] args){
+        int i = 20;
+        int s;
+        char a;
+        
+        s = sum(i, 10);
+        a = '?';
+        System.out.println(a);
+        System.out.print("hello");
+        System.out.print(s);
+   }
+}
